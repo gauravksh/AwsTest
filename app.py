7179fae11955800c508b83304ab1d20613832435
Gaurav Kumar <gauravksh1609@gmail.com>
2022-09-23 14:47:44 +0530

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def reva_home():
     return {"data":"It's created"}
 
+@app.route("/image")
+def access_image():
+    return render_template("home.html")
 if __name__ == "__main__":
     app.run(debug = True)
